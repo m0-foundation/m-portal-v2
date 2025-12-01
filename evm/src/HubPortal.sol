ef7b0b8469d006f366cb5d9e52f65076677a5c2f
0xIryna <iryna.hubernska@m0.xyz>
2025-12-01 13:15:22 -0800

    chore: remove unchecked

--- evm/src/HubPortal.sol
+++ evm/src/HubPortal.sol
@@ -271,11 +271,8 @@ contract HubPortal is Portal, HubPortalStorageLayout, IHubPortal {
         SpokeChainConfig storage spokeConfig = _getHubPortalStorageLocation().spokeConfig[destinationChainId];
         // Only track bridged principal for isolated Spokes
         if (spokeConfig.crossSpokeTokenTransferEnabled) return;
-
-        // Won't overflow since `getPrincipalAmountRoundedDown` returns uint112
-        unchecked {
-            spokeConfig.bridgedPrincipal += IndexingMath.getPrincipalAmountRoundedDown(uint240(amount), _currentIndex());
-        }
+        
+        spokeConfig.bridgedPrincipal += IndexingMath.getPrincipalAmountRoundedDown(uint240(amount), _currentIndex());
     }
 
     /// @dev Unlocks M tokens to `recipient`.
