dad8163ec2cc2fecd437775598272b357f605b3b
Jordan Hall <oighty6@gmail.com>
2025-12-23 14:53:14 -0600

    feat: send/receive cancel reports

--- evm/test/mocks/MockOrderBook.sol
+++ evm/test/mocks/MockOrderBook.sol
@@ -5,9 +5,14 @@ pragma solidity 0.8.30;
 import { IOrderBookLike } from "../../src/interfaces/IOrderBookLike.sol";
 
 contract MockOrderBook is IOrderBookLike {
-    FillReport public lastReport;
+    FillReport public lastFillReport;
+    CancelReport public lastCancelReport;
 
     function reportFill(FillReport calldata report) external {
-        lastReport = report;
+        lastFillReport = report;
+    }
+
+    function reportCancel(CancelReport calldata report) external {
+        lastCancelReport = report;
     }
 }
