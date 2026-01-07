6f72999e4a67a3316fa931c76ac157030e732202
0xIryna <iryna.hubernska@m0.xyz>
2026-01-07 15:14:37 -0800

    fix: fee-on-transfer tokens

---
+++ evm/test/fork/SpokePortal/sendToken.t.sol
@@ -0,0 +1,63 @@
+import { console } from "../../../lib/forge-std/src/console.sol";
+import {
+    IERC20
+} from "../../../lib/common/lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+import { TypeConverter } from "../../../src/libraries/TypeConverter.sol";
+import { PayloadType } from "../../../src/libraries/PayloadEncoder.sol";
+
+import { SpokePortalForkTestBase } from "./SpokePortalForkTestBase.sol";
+
+contract SendTokenForkTest is SpokePortalForkTestBase {
+    using TypeConverter for address;
+
+    bytes32 internal refundAddress = TOKEN_HOLDER.toBytes32();
+    bytes internal bridgeAdapterArgs = "";
+    bytes32 internal recipient = TOKEN_HOLDER.toBytes32();
+    uint256 internal amount = 1e6;
+
+    uint256 internal constant MAX_ROUNDING_ERROR = 2;
+
+    function test_sendToken_M() external {
+        uint256 mTotalSupplyBefore = IERC20(M_TOKEN).totalSupply();
+        uint256 userMBalanceBefore = IERC20(M_TOKEN).balanceOf(TOKEN_HOLDER);
+        uint256 fee = spokePortal.quote(ETHEREUM_CHAIN_ID, PayloadType.TokenTransfer);
+
+        vm.startPrank(TOKEN_HOLDER);
+        IERC20(M_TOKEN).approve(address(spokePortal), amount);
+        spokePortal.sendToken{ value: fee }(
+            amount, M_TOKEN, ETHEREUM_CHAIN_ID, M_TOKEN.toBytes32(), recipient, refundAddress, bridgeAdapterArgs
+        );
+        vm.stopPrank();
+
+        uint256 mTotalSupplyAfter = IERC20(M_TOKEN).totalSupply();
+        uint256 userMBalanceAfter = IERC20(M_TOKEN).balanceOf(TOKEN_HOLDER);
+
+        // $M is burnt on SpokePortal when sent to another chain
+        assertEq(mTotalSupplyAfter, mTotalSupplyBefore - amount);
+        assertEq(userMBalanceAfter, userMBalanceBefore - amount);
+    }
+
+    function test_sendToken_wM() external {
+        // Ensure SpokePortal has some $M to cover rounding errors when unwrapping Wrapped $M V1
+        assertGt(IERC20(M_TOKEN).balanceOf(address(spokePortal)), MAX_ROUNDING_ERROR);
+
+        uint256 mTotalSupplyBefore = IERC20(M_TOKEN).totalSupply();
+        uint256 userWrappedMBalanceBefore = IERC20(WRAPPED_M_TOKEN).balanceOf(TOKEN_HOLDER);
+        uint256 fee = spokePortal.quote(ETHEREUM_CHAIN_ID, PayloadType.TokenTransfer);
+
+        vm.startPrank(TOKEN_HOLDER);
+        IERC20(WRAPPED_M_TOKEN).approve(address(spokePortal), amount);
+        spokePortal.sendToken{ value: fee }(
+            amount, WRAPPED_M_TOKEN, ETHEREUM_CHAIN_ID, M_TOKEN.toBytes32(), recipient, refundAddress, bridgeAdapterArgs
+        );
+        vm.stopPrank();
+
+        uint256 mTotalSupplyAfter = IERC20(M_TOKEN).totalSupply();
+        uint256 userWrappedMBalanceAfter = IERC20(WRAPPED_M_TOKEN).balanceOf(TOKEN_HOLDER);
+
+        // $M is burnt on SpokePortal when sent to another chain
+        assertApproxEqAbs(mTotalSupplyAfter, mTotalSupplyBefore - amount, MAX_ROUNDING_ERROR);
+        assertEq(userWrappedMBalanceAfter, userWrappedMBalanceBefore - amount);
+    }
+}
---
+++ evm/test/mocks/MockFeeOnTransferExtension.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.30;
+
+import { IERC20 } from "../../lib/common/src/interfaces/IERC20.sol";
+
+import { MockERC20 } from "./MockERC20.sol";
+
+contract MockFeeOnTransferExtension is MockERC20 {
+    address public mToken;
+    uint256 public feeRate; // 100 = 1%, 10000 = 100%
+    address public feeRecipient;
+
+    constructor(address mToken_, uint256 feeRate_, address feeRecipient_) MockERC20("Mock Wrapped M", "Wrapped M", 6) {
+        mToken = mToken_;
+        feeRate = feeRate_;
+        feeRecipient = feeRecipient_;
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        uint256 fee = (amount * feeRate) / 10_000;
+        uint256 amountAfterFee = amount - fee;
+
+        balanceOf[msg.sender] -= amount;
+
+        unchecked {
+            balanceOf[to] += amountAfterFee;
+            balanceOf[feeRecipient] += fee;
+        }
+
+        emit Transfer(msg.sender, to, amountAfterFee);
+        if (fee > 0) emit Transfer(msg.sender, feeRecipient, fee);
+
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
+        uint256 fee = (amount * feeRate) / 10_000;
+        uint256 amountAfterFee = amount - fee;
+
+        uint256 allowed = allowance[from][msg.sender];
+
+        if (allowed != type(uint256).max) allowance[from][msg.sender] = allowed - amount;
+
+        balanceOf[from] -= amount;
+
+        unchecked {
+            balanceOf[to] += amountAfterFee;
+            balanceOf[feeRecipient] += fee;
+        }
+
+        emit Transfer(from, to, amountAfterFee);
+        if (fee > 0) emit Transfer(from, feeRecipient, fee);
+
+        return true;
+    }
+
+    function wrap(address recipient, uint256 amount) external {
+        IERC20(mToken).transferFrom(msg.sender, address(this), amount);
+        _mint(recipient, amount);
+    }
+
+    function unwrap(address recipient, uint256 amount) external {
+        _burn(msg.sender, amount);
+        IERC20(mToken).transfer(recipient, amount);
+    }
+}
